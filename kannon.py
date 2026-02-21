430fb63547244bd17e9296ed09bb4a113b045c38
aryan <aryansingh29052003@gmail.com>
2026-02-21 23:18:55 +0530

    reading /proc/:pid/stat for process information

--- kannon.py
+++ kannon.py
@@ -4,21 +4,36 @@
 import os
 import time
 import pwd
+import shutil
+
 
 while True:
     os.system("clear")
+    cols = shutil.get_terminal_size().columns
+    name_width = cols - 40
 
     # header
-    print(f"{'PID':>7} | {'USER':<12} | {'NAME':<20}")
-    print("-" * 45)
+    print(f"{'PID':>7} | {'USER':<12} | {'STATE':^5} | {'NAME':<{name_width}}")
+    print("=" * (cols - 1))
 
     for entry in sorted(
         os.listdir("/proc"), key=lambda x: int(x) if x.isdigit() else 0
     ):
         if entry.isdigit():
             try:
-                with open(f"/proc/{entry}/comm") as f:
-                    name = f.read().strip()
+                with open(f"/proc/{entry}/stat") as f:
+                    content = f.read()
+
+                first_paran = content.find("(")
+                last_paran = content.rfind(")")
+
+                # instead of using /proc/:pid/comm, we can just
+                # use /proc/:pid/stat to get it
+                name = content[first_paran + 1: last_paran]
+
+                fields = content[last_paran + 2:].split()
+                state = fields[0]
+                # utime = fields[11], stime = fields[12], starttime = fields[19]
 
                 with open(f"/proc/{entry}/status") as f:
                     for line in f:
@@ -30,13 +45,22 @@ while True:
 
                 try:
                     user = pwd.getpwuid(uid).pw_name
+
+                    if len(user) > 12:
+                        user = user[:11] + "…"
+                    if len(name) > name_width:
+                        name = name[:name_width - 1] + "…"
+
                     # print(f">>> pwd entry: {pwd.getpwuid(uid)}")
                 except KeyError:
                     user = str(uid)
 
-                print(f"{entry:>7} | {user:<12} | {name:<20}")
+                print(f"{entry:>7} | {user:<12} | {state:^5} | {name:<20}")
 
-            except (PermissionError, FileNotFoundError):
+            except (PermissionError, FileNotFoundError, ValueError, IndexError):
                 pass
-    print("=" * 50)
+    print("=" * (cols - 1))
+    # im not using all states specified in manfile `proc_pid_stat(5)`
+    # these should suffice for now i believe
+    print("States: R=Running S=Sleeping D=Disk Z=Zombie T=Stopped")
     time.sleep(1)
